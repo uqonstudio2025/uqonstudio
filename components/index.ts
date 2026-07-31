b7b0ae09839d73a04ac6d9659f4964c001a7730b
uqonstudio2025 <sdesain25@gmail.com>
2026-08-01 04:22:52 +0700

    Add files via upload

---
+++ components/index.ts
@@ -0,0 +1,43 @@
+/**
+ * Layout
+ */
+
+export * from "./layout";
+
+/**
+ * Motion
+ */
+
+export * from "./motion";
+
+/**
+ * UI
+ */
+
+export * from "./ui";
+
+/**
+ * Sections
+ */
+
+export * from "./hero";
+
+export * from "./about";
+
+export * from "./services";
+
+export * from "./portfolio";
+
+export * from "./workflow";
+
+export * from "./whyChoose";
+
+export * from "./testimonials";
+
+export * from "./faq";
+
+export * from "./contact";
+
+export * from "./footer";
+
+export * from "./layout/navbar";
